064ff77db37c6fc4c54b33f0b7dc1d200fc78dfc
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2018-11-16 20:23:55 -0500

    Getting user input

--- project2.s
+++ project2.s
@@ -3,3 +3,11 @@
 	emptyInputError: .asciiz "Empty input"
 	baseError: .asciiz "Invalid base-34 number"
 	userInput: .space 30000
+.text
+	main:
+	
+		#	--INPUT--     #
+		#Get user input 
+		li $v0, 8
+		la $a0, userInput
+		li $a1, 30000
