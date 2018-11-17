167edb7203e2ba896b89798878b3c558dd862166
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2018-11-16 20:25:48 -0500

    Dealing with space manipulation
    For before and after user input

--- project2.s
+++ project2.s
@@ -11,3 +11,10 @@
 		li $v0, 8
 		la $a0, userInput
 		li $a1, 30000
+		syscall
+		
+		#Dealing with space manipulation#
+		removeSpaces:
+			li $t8, 32
+			lb $t9, 0($a0)
+			beq $t8, $t9, removeFirst
